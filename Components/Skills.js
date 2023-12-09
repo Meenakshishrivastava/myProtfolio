fb32fde81eb22ef9bccd88c8508ac978f6e5458b
Meenakshi Shrivastava <140409432+Meenakshishrivastava@users.noreply.github.com>
2023-12-10 02:39:39 +0530

    Update Skills.js

--- Components/Skills.js
+++ Components/Skills.js
@@ -46,10 +46,6 @@ const Skills = () => {
                 <p className='my-4'>TAILWIND CSS</p>
               </div>
 
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src='./images/aws.png' alt="HTML icon"/>
-                <p className='my-4'>AWS</p>
-              </div>
 
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src='./images/github.png' alt="HTML icon"/>
