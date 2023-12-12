88efd993d323568c04839d1d37ba6fa7c6590dcf
Meenakshi Shrivastava <140409432+Meenakshishrivastava@users.noreply.github.com>
2023-12-12 21:43:20 +0530

    Update Home.js

--- Components/Home.js
+++ Components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 import {Link} from 'react-scroll'
+import {TypeAnimation} from 'react-type-animation'
 const Home = () => {
   return (
     
@@ -13,11 +14,21 @@ const Home = () => {
    
 
         {/* container*/}
-        <div className='bg-violet-950 max-auto items-center px-8 flex flex-col justify-center h-full '>
+        <div className='bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 max-auto items-center px-8 flex flex-col justify-center h-full '>
         <img src='./images/icon3.gif'/>
             <p className='text-black-600'>Hi, my name is</p>
             <h1 className='text-4xl sa:text-7xl font-bold text-[#f2f3f5]'>Meenakshi Shrivastava</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#f3f3f5]'>I'm a Full-stack developer.</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#f3f3f5]'><h2 >I'm a</h2>
+            <div> 
+              <TypeAnimation sequence={[
+              'Full-stack developer.', 
+               2000,
+              'MERN stack developer.',
+               2000,
+              'Web desiger.',
+               2000,
+              ]}/>
+              </div></h2>
             <p className='text-black py-4 max-w-[700px]'> I'm a Full-stack developer specializing in building (and occasionally Designing) exceptional digital expriences. Currently, i'm focused on building responsive full-stack web applications.</p>
         
     <div>
