a91bd5884ed39146c3837ac150f0c8fb69397906
Meenakshi Shrivastava <140409432+Meenakshishrivastava@users.noreply.github.com>
2024-04-27 07:38:09 -0700

    Update Navbar.js

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 flex flex-col justify-center items-center'
+            : 'absolute top-0 left-0 w-full h-screen bg-teal-600 flex flex-col justify-center items-center'
         }
       >
         <li className='py-6 text-4xl'>
